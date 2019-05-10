15df2ae652038d1366765e91235767effe15cdf8
Annaluise van Delden <hello@annaluise.org>
2019-05-10 13:41:25 +0200

    Move conig files away from home to their respective dot directories.

--- profile_helper.fish
+++ profile_helper.fish
@@ -14,8 +14,8 @@
 set SCRIPT_DIR (realpath (dirname (status -f)))
 
 # load currently active theme...
-if test -e ~/.base16_theme
-  eval sh '"'(realpath ~/.base16_theme)'"'
+if test -e ~/.config/base16-shell/base16_theme
+  eval sh '"'(realpath ~/.config/base16-shell/base16_theme)'"'
 end
 
 
@@ -24,9 +24,9 @@ for SCRIPT in $SCRIPT_DIR/scripts/*.sh
   set THEME (basename $SCRIPT .sh)
   function $THEME -V SCRIPT -V THEME
     eval sh '"'$SCRIPT'"'
-    ln -sf $SCRIPT ~/.base16_theme
+    ln -sf $SCRIPT ~/.config/base16-shell/base16_theme
     set -x BASE16_THEME (string split -m 1 '-' $THEME)[2]
-    echo -e "if !exists('g:colors_name') || g:colors_name != '$THEME'\n  colorscheme $THEME\nendif" >  ~/.vimrc_background
+    echo -e "if !exists('g:colors_name') || g:colors_name != '$THEME'\n  colorscheme $THEME\nendif" >  ~/.vim/vimrc_background
   end
 end
 
